7022de060b382f1f4b0b48c3470f2ee75925d24a
psalzy08 <1203573@spsd.sk.ca>
2025-10-21 14:09:51 -0600

    Update sketch.js

--- Traffic Simulation/sketch.js
+++ Traffic Simulation/sketch.js
@@ -3,15 +3,18 @@
 // October 20th, 2025
 //
 
-
-
+let myCar;
+let direction;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  myCar = new Car(300, 1);
 }
 
 function draw() {
   background(220);
   drawRoad();
+  myCar.display();
+  myCar.move();
 }
 function drawRoad(){
   //creates the road
@@ -27,7 +30,31 @@ function drawRoad(){
   }
 }
 class Car{
-  constructor(x,y){
+  constructor(y,direction){
+    this.x = random(width); this.y = y;
+    this.c = color(random(255), random(255), random(255));
+    this.s = random(2,10);
+    this.direction = random(2);
+  }
+  display(){
+    rectMode(CORNER);
+    fill(this.c);
+    noStroke();
+    rect(this.x, this.y, 50, 30);
+    stroke(2);
+    fill(255);
+    rect(this.x + 5, this.y, 10, -5 )
+    rect(this.x + 35, this.y, 10, -5 )
+    rect(this.x + 35, this.y + 35, 10, -5 )
+    rect(this.x + 5, this.y + 35, 10, -5 )
+  }
+  move(){
+    if (this.direction = 1){
+      this.x += this.s;
+    }
     
+    if(this.x > width){
+      this.x = 0;
+    }
   }
 }
