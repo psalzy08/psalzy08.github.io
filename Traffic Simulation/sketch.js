f20c814cf5059ff78c4aad3b5bf36b587fbc4617
psalzy08 <1203573@spsd.sk.ca>
2025-10-22 14:08:59 -0600

    Update sketch.js

--- Traffic Simulation/sketch.js
+++ Traffic Simulation/sketch.js
@@ -3,18 +3,20 @@
 // October 20th, 2025
 //
 
-let myCar;
+let myVehicle;
 let direction;
+let eastbound = [];
+let westbound = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  myCar = new Car(300, 1);
+  myVehicle = new Vehicle(1, round(random(0,1)));
 }
 
 function draw() {
   background(220);
   drawRoad();
-  myCar.display();
-  myCar.move();
+  myVehicle.display();
+  myVehicle.move();
 }
 function drawRoad(){
   //creates the road
@@ -29,32 +31,54 @@ function drawRoad(){
 
   }
 }
-class Car{
-  constructor(y,direction){
-    this.x = random(width); this.y = y;
+class Vehicle{
+  constructor(direction, type){
+    this.x = random(width); this.y = random(250, 430);
     this.c = color(random(255), random(255), random(255));
-    this.s = random(2,10);
-    this.direction = random(2);
+    this.xSpeed = random(2,10);
+    this.direction = direction;
+    this.type = type;
   }
   display(){
-    rectMode(CORNER);
-    fill(this.c);
-    noStroke();
-    rect(this.x, this.y, 50, 30);
-    stroke(2);
-    fill(255);
-    rect(this.x + 5, this.y, 10, -5 )
-    rect(this.x + 35, this.y, 10, -5 )
-    rect(this.x + 35, this.y + 35, 10, -5 )
-    rect(this.x + 5, this.y + 35, 10, -5 )
+    if(this.type === 0){
+      rectMode(CORNER);
+      fill(this.c);
+      noStroke();
+      rect(this.x, this.y, 50, 30);
+      stroke(2);
+      fill(255);
+      rect(this.x + 5, this.y, 10, -5 );
+      rect(this.x + 35, this.y, 10, -5 );
+      rect(this.x + 35, this.y + 35, 10, -5 );
+      rect(this.x + 5, this.y + 35, 10, -5 );
+    }
+    if(this.type === 1){
+      rectMode(CORNER);
+      fill(this.c);
+      if(this.direction === 1){
+        rect(this.x, this.y, 20, 40);
+        rect(this.x + 25, this.y, 40, 40);
+      }
+      else{
+        rect(this.x, this.y, 40, 40);
+        rect(this.x + 45, this.y, 20, 40);
+      }
+    }
   }
   move(){
-    if (this.direction = 1){
-      this.x += this.s;
+    if (this.direction === 1){
+      this.x -= this.xSpeed;
+      if(this.x < -100){
+        this.x = width + 400;
+        
+      }
     }
-    
-    if(this.x > width){
-      this.x = 0;
-    }
+    if (this.direction === 2){
+      
+      this.x += this.xSpeed;
+      if(this.x > width){
+        this.x = -400;
+      }
+    }  
   }
 }
