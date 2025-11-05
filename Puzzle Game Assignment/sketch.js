ce21200e01fb6f0253fbfd41e703ae8aa490b409
psalzy08 <1203573@spsd.sk.ca>
2025-11-05 14:10:31 -0600

    .

--- Puzzle Game Assignment/sketch.js
+++ Puzzle Game Assignment/sketch.js
@@ -1,16 +1,17 @@
-// 2D Array Basics
+// Puzzle Game Assignment
 // Peyton Salzsauler
-// November 3rd, 2025
+// November 4th, 2025
 //
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
 
-
-// grid is 5x4 in dimension
 let grid = [
-  [0,    0,  0, 255,  0 ],
-  [255,  0, 255, 0,  255],
-  [255, 255, 0, 255, 255],
-  [0,   255, 0,  0 ,  0 ],
-];
+  [0 ,255, 255, 0, 0],
+  [0 , 255 , 0, 255, 0],
+  [255, 0, 0, 255, 255],
+  [0, 255, 255, 0, 255],
+]
+
 
 let rows = grid.length;
 let cols = grid[0].length;
@@ -29,42 +30,42 @@ function draw() {
 
 function mousePressed(){
   //flip current tile
-  // upgrade: only do this if mouse is on the canvas
-
+  //upgrade: only do this if the mouse is on Canvas
+  
   let x = getCurrentX();
   let y = getCurrentY();
-  
-  // always flip the focused tile
+
+  //ALWAYS: flip the "focused" tile
   flip(x,y);
 
-  if(x+1 < cols) flip(x+1, y);
-  if(y-1 >= 0) fill(x, y - 1);
-  if(x -1 >= 0) fill(x - 1 );
-  
- 
+  //IF THEY EXIST:
+  //flip our NSEW neighbours (cross pattern)
+  if(x+1 < cols) flip(x+1,y);
+  if(y-1 >= 0) flip(x, y-1);
+  if(y+1 < rows) flip(x, y+1);
+  if(x-1 >= 0) flip(x-1, y);
+
 }
 
 function getCurrentX(){
-  // determine current col of mouse position
+  //determine current col of mouse position
   let constrainedX = constrain(mouseX, 0, width-1);
   return floor(constrainedX / squareSize);
 }
 
 function getCurrentY(){
-  // determine current col of mouse position
+  //determine current row of mouse position
   let constrainedY = constrain(mouseY, 0, height-1);
   return floor(constrainedY / squareSize);
 }
-
 function flip(x,y){
-  // takes a tile @ x,y and inverts its value
+  //takes a tile @ x,y and inverts its value
   if(grid[y][x] === 0) grid[y][x] = 255;
   else grid[y][x] = 0;
 }
 
-
 function renderGrid(){
-  // interpret the information in the 2d array, and draw
+  // interpret the information in the 2D array, and draw
   // a grid of square on the screen to reflect it.
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
@@ -73,4 +74,8 @@ function renderGrid(){
       square(x*squareSize,y*squareSize,squareSize);
     }
   }
+}
+function randomGrid(){
+  // create a grid that randomizes every reset
+  
 }
